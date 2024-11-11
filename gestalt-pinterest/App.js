1ac31dd651a2ec68741969b69404d621817186f2
Kalyani Krishna <132040547+kalyanikrishna465@users.noreply.github.com>
2024-11-11 22:10:39 +0530

    Create App.js

---
+++ gestalt-pinterest/App.js
@@ -0,0 +1,31 @@
+// App.js
+import React from 'react';
+import Header from './components/Header';
+import Contents from './components/Contents';
+import Footer from './components/Footer';
+import Text from './components/Text';
+
+function App() {
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'row', // Ensure components are aligned horizontally
+    gap: '0px',
+  };
+
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div style={containerStyle}>  
+        <Text />
+        <Contents />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
